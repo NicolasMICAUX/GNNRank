d4cc3c9da1d0a6d24065f1372ed3cbeded2dab18
NicolasMICAUX <micaux.nicolas@gmail.com>
2023-03-19 18:34:26 +0100

    requirements.txt specify url of binary distribution to make install faster

---
+++ special_requirements.sh
@@ -0,0 +1,3 @@
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.13.1+cu116.html --no-index
+pip install torch-geometric -f https://pytorch-geometric.com/whl/torch-1.13.1+cu116.html --no-index
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.13.1+cu116.html --no-index
