005af3e387f95b571e6266edb12b09e4f3a3a708
NicolasMICAUX <micaux.nicolas@gmail.com>
2023-03-25 16:44:52 +0100

    Compute upset metrics on tournesol ground truth

---
+++ data/tournesol/compute_metrics.py
@@ -0,0 +1,21 @@
+"""Compute `upset_naive` and `upset_simple` on Tournesol "ground truth"."""
+import scipy
+import numpy as np
+import torch
+from GNNRank.src.metrics import calculate_upsets
+
+
+# load data adj.npz and y.npy
+A = scipy.sparse.load_npz('adj.npz')
+score = np.load('y.npy')
+
+# Convert to FloatTensor
+A = torch.from_numpy(A.toarray()).float()
+score = torch.from_numpy(score).float()
+score = score.unsqueeze(1)
+
+
+upset_simple = calculate_upsets(A, score, style='simple')
+upset_naive = calculate_upsets(A, score, style='naive')
+print('upset_simple', upset_simple.item())
+print('upset_naive', upset_naive.item())
